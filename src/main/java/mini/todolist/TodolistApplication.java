e0655b29834f994cf32b151d4184357865c6e02b
bookpark <bkbkgg@gmail.com>
2022-11-17 01:02:39 +0900

    서버가 시작될 때 객체를 10개 생성하는 코드 작성

--- src/main/java/mini/todolist/TodolistApplication.java
+++ src/main/java/mini/todolist/TodolistApplication.java
@@ -1,7 +1,13 @@
 package mini.todolist;
 
+import mini.todolist.domain.Todo;
+import mini.todolist.repository.TodoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class TodolistApplication {
@@ -9,5 +15,16 @@ public class TodolistApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
+
+//	@Bean
+//	public CommandLineRunner runner(TodoRepository todoRepository) throws Exception {
+//		return (args) -> {
+//			IntStream.rangeClosed(1, 10).forEach(index -> todoRepository.save(Todo.builder()
+//					.content("스프링 공부" + index)
+//					.isComplete(false)
+//					.build())
+//			);
+//		};
+//	}
 
 }
