e022056c8678a035b56d15a25a58979e6261cf56
bookpark <bkbkgg@gmail.com>
2022-11-17 00:31:45 +0900

    기본 도메인 구현

--- src/main/java/mini/todolist/domain/Todo.java
+++ src/main/java/mini/todolist/domain/Todo.java
@@ -1,25 +1,43 @@
 package mini.todolist.domain;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Getter
 @Setter
-public class Todo {
+@NoArgsConstructor
+@Table
+public class Todo implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private Boolean completed;
-
     @Column(nullable = false, length = 200)
-    private String todoName;
+    private String content;
 
+    @Column(nullable = false)
+    private Boolean isComplete;
+
+    @Builder
+    public Todo(Long id, String content, Boolean isComplete) {
+        this.id = id;
+        this.content = content;
+        this.isComplete = isComplete;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo [id=" + id +
+                ", content=" + content +
+                ", isComplete=" + isComplete + "]";
+    }
 }
